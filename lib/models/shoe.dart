fd5a884f542a5a03bccea0ff3cd3d556b5a3467a
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2024-01-22 10:15:32 +0200

    shoe class

---
+++ lib/models/shoe.dart
@@ -0,0 +1,27 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Shoe {
+  Shoe({
+    required this.brand,
+    this.description,
+    required this.image,
+    this.lastPrice,
+    required this.name,
+    required this.price,
+  })  : createdAt = DateTime.now(),
+        id = uuid.v4();
+  final String brand;
+  final DateTime createdAt;
+  final String? description;
+  final String id;
+  final String image;
+  final double? lastPrice;
+  final String name;
+  final double price;
+}
+
+List<Shoe> Shoes=[
+  Shoe(brand: 'Nike', image: image, name: name, price: price)
+]
