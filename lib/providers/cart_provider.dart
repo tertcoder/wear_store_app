c42fe717aa1f6283ed02f7661c40d980eb9bef4f
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2024-02-08 20:56:50 +0200

    Cart Provider States

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wear_store_app/models/shoe.dart';
+
+class CartNotifier extends StateNotifier<List<Shoe>> {
+  CartNotifier() : super([]);
+
+  bool toogleInCart(Shoe shoe) {
+    if (state.contains(shoe)) {
+      state = state.where((el) => el.id != shoe.id).toList();
+      return false;
+    } else {
+      state = [...state, shoe];
+      return true;
+    }
+  }
+
+  bool isInCart(Shoe shoe) {
+    if (state.contains(shoe)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
