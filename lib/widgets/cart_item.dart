c2c165afa5a6350a0d3e0d6ae91ff13319902dc2
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2024-02-08 20:59:00 +0200

    Cart Item Widget

---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:wear_store_app/widgets/shadow_main.dart';
+
+class CartItem extends StatelessWidget {
+  const CartItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ShadowMain(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        width: double.infinity,
+        height: 100,
+        margin: const EdgeInsets.symmetric(horizontal: 24),
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.fromBorderSide(
+            BorderSide(
+              color: Theme.of(context).colorScheme.outline,
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+            Image.asset(
+              "assets/images/shoes/image-7.webp",
+              width: 100,
+              height: 100,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            const Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("Shoe 01"),
+                      Text("\$80.87"),
+                      Text("1 pair"),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text("X"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/my_cart.dart
+++ lib/widgets/my_cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wear_store_app/widgets/cart_item.dart';
 import 'package:wear_store_app/widgets/pushed_screen_app_bar.dart';
 import 'package:wear_store_app/widgets/shadow_main.dart';
 
@@ -13,62 +14,9 @@ class MyCart extends StatelessWidget {
         title: "My Cart",
       ),
       body: ListView(
-        children: [
-          const SizedBox(height: 16),
-          ShadowMain(
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              width: double.infinity,
-              height: 100,
-              margin: const EdgeInsets.symmetric(horizontal: 24),
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.fromBorderSide(
-                  BorderSide(
-                    color: Theme.of(context).colorScheme.outline,
-                  ),
-                ),
-              ),
-              child: Row(
-                children: [
-                  Image.asset(
-                    "assets/images/shoes/image-7.webp",
-                    width: 100,
-                    height: 100,
-                    fit: BoxFit.cover,
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text("Shoe 01"),
-                            Text("\$80.87"),
-                            Text("1 pair"),
-                          ],
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Text("X"),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+        children: const [
+          SizedBox(height: 16),
+          CartItem(),
         ],
       ),
     );
