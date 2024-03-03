b45be65074cfec18985493e205c6c7fc19e6212f
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2024-03-03 05:13:40 +0200

    updating the splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class MyApp extends StatelessWidget {
           if (snapshot.hasData) {
             return const BottomNavBar();
           }
-          return const WelcomeScreen();
+          return const SplashScreen();
         },
       ),
     );
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 1),
+    vsync: this,
+  )..repeat(reverse: false);
+
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.linear,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +34,18 @@ class SplashScreen extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  width: 32,
-                  height: 12,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFB923C),
-                    borderRadius: BorderRadius.circular(100),
-                    border: const Border.fromBorderSide(
-                      BorderSide(
-                        color: Color(0xFF28231c),
+                RotationTransition(
+                  turns: _animation,
+                  child: Container(
+                    width: 32,
+                    height: 12,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFB923C),
+                      borderRadius: BorderRadius.circular(100),
+                      border: const Border.fromBorderSide(
+                        BorderSide(
+                          color: Color(0xFF28231c),
+                        ),
                       ),
                     ),
                   ),
